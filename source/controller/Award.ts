b9bdeba7854f96d08c20e8b1a382d8d0f8b343ee
luojiyin <luojiyin@hotmail.com>
2024-08-14 17:55:37 +0800

    save

---
+++ source/controller/Award.ts
@@ -0,0 +1,48 @@
+import { createHash } from 'crypto';
+import { JsonWebTokenError, sign } from 'jsonwebtoken';
+import {
+    Authorized,
+    Body,
+    CurrentUser,
+    Delete,
+    ForbiddenError,
+    Get,
+    JsonController,
+    OnNull,
+    OnUndefined,
+    Param,
+    Post,
+    Put,
+    QueryParams
+} from 'routing-controllers';
+import { ResponseSchema } from 'routing-controllers-openapi';
+
+import {
+    Award,
+    dataSource,
+    Hackathon,
+    JWTAction,
+    Role,
+    SignInData,
+    User,
+    UserFilter,
+    UserListChunk
+} from '../model';
+import { APP_SECRET, searchConditionOf } from '../utility';
+
+
+@JsonController('/hackathon/:hackathon/award/:award')
+export class AwardController{
+    store = dataSource.getRepository(Award);
+    hackathonStore = dataSource.getRepository(Hackathon);
+    
+    @Put('/:uid')
+    @Authorized()
+    @ResponseSchema(Award)
+    async createOne(
+        @Param('hackathon') hackathon:string, 
+        @Body() body:
+    )
+
+
+}
